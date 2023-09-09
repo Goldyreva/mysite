3c85502e4df6f230251df8a0be311792bd4c9cb7
elizaveta <liza.gold.omsk@gmail.com>
2023-09-09 22:28:34 +0300

    - init

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import s from './Header.module.css'
+
 const Header = () => {
     return (
         <div className={s.header}>
             <div className={s.header__description}>
                 <h1>Привет! Познакомимся?</h1>
                 <p> Я - Лиза, мне 22 года и я занимаюсь веб - разработкой. На этом сайте вы можете узнать небольшую историю моего увлечения программированием и посмотреть видеоурок по анимации на CSS.</p>
-                <a href="#" className={s.header__description__link}>Ссылка на репозиторий с этим сайтом</a>
+                <a href="https://github.com/Goldyreva/mysite" className={s.header__description__link}>Ссылка на репозиторий с этим сайтом</a>
             </div>
             <div className={s.header__links}>
                 <a href="#gallery" className={s.header__links__link}>Галерея</a>
